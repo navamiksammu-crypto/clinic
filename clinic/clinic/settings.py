aaf825f16a1eed65774120de3a976273a24faf78
navamiksammu-crypto <navamiksammu@gmail.com>
2026-06-19 23:01:42 -0800

    Update settings.py

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+1aw+5vsgowf_rguf7%6=&%i9g#o2ljs+i8e)ie1#g0l7z2&+-
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://clinic-1-flf2.onrender.com',
+    'clinic-1-flf2.onrender.com',
 ]
 
 
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
